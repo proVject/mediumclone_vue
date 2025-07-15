17abaa735d1e50d9fa89d9a4315c2e2f07d3c298
vovazb <vovazb@connect.net.il>
2025-07-15 19:25:30 +0300

    update api endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import persistenceStorage from '@/helpers/persistenceStorage'
 
-axios.defaults.baseURL = 'https://realworld-api.vovazb.art/'
+axios.defaults.baseURL = 'https://realworld-api.vovazb.art/api'
 
 axios.interceptors.request.use((config) => {
   const token = persistenceStorage.getItem('accessToken')
